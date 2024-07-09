1f9d68a9ad2d61bcb4a9ef0f6d8c411dccff4fe0
Mischa Holz <me@mhlz.de>
2024-07-09 19:39:30 +0200

    Reverse order of parameters

--- src/canvas.js
+++ src/canvas.js
@@ -1,61 +1,73 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 
 // { x,y, height, width, handler)
-const handlers = []
+const handlers = [];
 
 /**
  * Register a click handler for the given bounds
  * Will be called when a click event is received on the canvas, that is within the given bounds
  * @param x position from the left side of the canvas
- * @param width width of the clickable area
  * @param y position from the top side of the canvas
+ * @param width width of the clickable area
  * @param height height of the clickable area
  * @param handler function to be called when the click event is received
  */
-export function onClick(x, width, y, height, handler) {
-    handlers.push({
-        x,
-        width,
-        y,
-        height,
-        handler
-    })
+export function onClick(x, y, width, height, handler) {
+  handlers.push({
+    x,
+    y,
+    width,
+    height,
+    handler,
+  });
 }
 
 /**
  * Remove all click handlers
  */
 export function resetOnClicks() {
-    handlers.splice(0, handlers.length)
+  handlers.splice(0, handlers.length);
 }
 
-const elemLeft = canvas.offsetLeft + canvas.clientLeft
-const elemTop = canvas.offsetTop + canvas.clientTop
+const elemLeft = canvas.offsetLeft + canvas.clientLeft;
+const elemTop = canvas.offsetTop + canvas.clientTop;
 
-const debug = false
+const debug = false;
 
-canvas.addEventListener('click', function (event) {
-    const x = event.pageX - elemLeft
-    const y = event.pageY - elemTop
+canvas.addEventListener(
+  "click",
+  function (event) {
+    const x = event.pageX - elemLeft;
+    const y = event.pageY - elemTop;
 
     if (debug) {
-        console.log(`received click on x: ${x}, y: ${y}`)
+      console.log(`received click on x: ${x}, y: ${y}`);
     }
-    let called = false
+    let called = false;
 
     // Collision detection between clicked offset and element.
     // Go through all registered handlers and call all that matches the click
     handlers.forEach(function (element) {
-        if (y > element.y && y < element.y + element.height && x > element.x && x < element.z + element.width) {
-            called = true
-            if (debug) {
-                console.log(`calling handler for x: ${x}, y: ${y}, with bounds x: ${element.x}, y: ${element.y}, width: ${element.width}, height: ${element.height}`)
-            }
-            element.handler()
+      if (
+        y > element.y &&
+        y < element.y + element.height &&
+        x > element.x &&
+        x < element.z + element.width
+      ) {
+        called = true;
+        if (debug) {
+          console.log(
+            `calling handler for x: ${x}, y: ${y}, with bounds x: ${element.x}, y: ${element.y}, width: ${element.width}, height: ${element.height}`,
+          );
         }
-    })
+        element.handler();
+      }
+    });
 
     if (!called && debug) {
-        console.log(`no handler for x: ${x}, y: ${y} found, ignoting click`)
+      console.log(`no handler for x: ${x}, y: ${y} found, ignoting click`);
     }
-}, false);
+  },
+  false,
+);
+

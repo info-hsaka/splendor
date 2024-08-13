121e1ad5dc2b597de0d8963c49e78e42803a6ee7
DESKTOP-U26SPHU\tn <jan+2@opencreek.tech>
2024-08-13 10:01:42 +0200

    Innit

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,10 @@
 import { TurnOrder } from "boardgame.io/core";
 
-const Seltenheit3 = [{farbe: grün, Siegpunkte: 4, Preis: {rot:0, grün:0, blau:7, weiß: 0, schwarz:0}}]
+const Seltenheit1 = [{farbe: "blau", Siegpunkte: 2, Preis: {rot:1, gruen:3, blau:2, weiß: 0, schwarz:0}}]
+const Seltenheit2 = [{ farbe: "rot", Siegpunkte: 3, Preis: { rot: 0, gruen: 0, blau: 5, weiß: 3, schwarz: 1 } }]
+const Seltenheit3 = [{ farbe: "gruen", Siegpunkte: 4, Preis: { rot: 0, gruen: 0, blau: 7, weiß: 0, schwarz: 0 } }]
+
+console.log(Seltenheit1)
 
 export const Game = {
   setup: (setup) => {

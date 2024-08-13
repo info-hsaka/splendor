8f9935adcc5914665d6a436c676f4626072e076b
DESKTOP-U26SPHU\tn <jan+2@opencreek.tech>
2024-08-13 09:48:50 +0200

    Karten modellieren

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,7 @@
 import { TurnOrder } from "boardgame.io/core";
 
+const Seltenheit3 = [{farbe: grün, Siegpunkte: 4, Preis: {rot:0, grün:0, blau:7, weiß: 0, schwarz:0}}]
+
 export const Game = {
   setup: (setup) => {
     return {};
@@ -27,5 +29,8 @@ export const Game = {
 
   disableUndo: true,
 
-  endIf: (endIf) => {},
+  endIf: (endIf) => { },
+  
+
+
 };

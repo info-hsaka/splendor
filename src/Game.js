02e504b8cd02c233fc274e829a1d3d02b2654417
DESKTOP-U26SPHU\tn <jan+2@opencreek.tech>
2024-08-13 10:19:51 +0200

    nobles

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,12 @@
 import { TurnOrder } from "boardgame.io/core";
 
-const Seltenheit1 = [{farbe: "blau", Siegpunkte: 2, Preis: {rot:1, gruen:3, blau:2, weiß: 0, schwarz:0}}]
-const Seltenheit2 = [{ farbe: "rot", Siegpunkte: 3, Preis: { rot: 0, gruen: 0, blau: 5, weiß: 3, schwarz: 1 } }]
-const Seltenheit3 = [{ farbe: "gruen", Siegpunkte: 4, Preis: { rot: 0, gruen: 0, blau: 7, weiß: 0, schwarz: 0 } }]
+// Karten
+const Seltenheit1 = [{farbe: "blau", Siegpunkte: 2, Preis: {rot:1, gruen:3, blau:2, weiss: 0, schwarz:0}}]
+const Seltenheit2 = [{ farbe: "rot", Siegpunkte: 3, Preis: { rot: 0, gruen: 0, blau: 5, weiss: 3, schwarz: 1 } }]
+const Seltenheit3 = [{ farbe: "gruen", Siegpunkte: 4, Preis: { rot: 0, gruen: 0, blau: 7, weiss: 0, schwarz: 0 } }]
 
-console.log(Seltenheit1)
+// Nobles
+const nobles = [{Siegpunkte: 3, Preis: {rot: 0, gruen: 4, blau: 4, weiss: 0, schwarz: 0}}]
 
 export const Game = {
   setup: (setup) => {

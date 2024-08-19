bdf6d5fd2e4ab913f4e918ecd0fb096cd686f823
Hilmar Wiegand <me@hwgnd.de>
2024-08-19 10:59:42 +0200

    Adjust server id generation

--- src/Server.js
+++ src/Server.js
@@ -1,12 +1,26 @@
-import { Server } from "boardgame.io/server";
-import { Game } from "./Game";
-import serve from "koa-static";
+import { Server } from "boardgame.io/server"
+import serve from "koa-static"
+import { Cascadia } from "./cascadia"
+
+function makeId(length) {
+  let result = ""
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  const charactersLength = characters.length
+  let counter = 0
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+  return result
+}
 
 const server = Server({
-    games: [Game],
-    origins: [/.*/],
-});
+  games: [Cascadia],
+  origins: [/.*/],
+  uuid: () => makeId(6),
+  generateCredentials: () => makeId(64),
+})
 
-server.app.use(serve("dist"));
+server.app.use(serve("dist"))
 
-server.run(process.env.PORT ?? 8000);
+server.run(process.env.PORT ?? 8000)
